185f97d70365c062162f9c5f0d8e9afdfd527911
1101137152 <1101137152@gm.kuas.edu.tw>
2015-03-30 19:15:45 +0800

    First commit

---
+++ hw0327/WebForm2.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// ExecuteReader, ExecuteNonQuery, ExecuteScalar
+    /// </summary>
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnSearch_Click(null, null);
+        }
+
+        /// <summary>
+        /// ExecuteReader
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["studentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Studentdata where StudentName like @name";
+                    cmd.Parameters.Add(new SqlParameter("@name", "%" + txtSearch.Text + "%"));
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(dr);
+                        gvResult.DataSource = dt;
+                        gvResult.DataBind();
+                        dr.Close();
+                    }
+                }
+            }
+        }
+
+        //ExecuteScalar
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string sql = @"INSERT INTO [dbo].[Studentdata]([StudentName],[StudentPhone],[StudentAddress])
+                             VALUES
+                               (@Name
+                               ,@Phone
+                               ,@Address);SELECT CAST(scope_identity() AS int);";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["studentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@Name", txtName.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Phone", txtPhone.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Address", TextAddress.Text));
+
+                    txtID.Text = cmd.ExecuteScalar().ToString();
+                }
+                btnSearch_Click(null, null);
+            }
+        }
+
+        /// <summary>
+        /// ExecuteNonQuery
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            string sql = @"update [Studentdata] set [StudentName] = @Name, [StudentPhone] = @Phone, [StudentAddress] = @Address
+                             where StudentId = @ID";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["studentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@ID", txtE_ID.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Name", txtE_Name.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Phone", txtE_Phone.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Address", txtE_Address.Text));
+
+                    cmd.ExecuteNonQuery();
+                }
+                btnSearch_Click(null, null);
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["studentConnectionString"].ConnectionString);
+
+            string sqlStatement = "DELETE FROM Studentdata WHERE StudentId = @StudentId";
+
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sqlStatement, connection);
+                cmd.Parameters.AddWithValue("@StudentId", txtD_ID.Text);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                connection.Close();
+                btnSearch_Click(null, null);
+            }
+        }
+    }
+}
---
+++ hw0327/WebForm3.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using YEAR;
+namespace WebApplication1
+{
+    public partial class WebForm7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnCal_Click(object sender, EventArgs e)
+        {
+            Class1 cal = new Class1();
+            int inputAge = 0;
+            Class1.YearFormat inputFormat = (Class1.YearFormat)Enum.Parse(typeof(Class1.YearFormat), ddlYearFormat.SelectedValue, false);
+
+            int.TryParse(txtAge.Text, out inputAge);
+            if (inputAge > 0)
+            {
+                lblYear.Text = cal.GetBirthYear(inputAge, inputFormat).ToString();
+            }
+        }
+    }
+}

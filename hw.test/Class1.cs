1fb3c49440b82e9cd52224842fccca114728724a
1101137152 <1101137152@gm.kuas.edu.tw>
2015-04-09 16:29:24 +0800

    third commit

---
+++ hw.test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApplication1.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void CalYear()
+        {
+            YEAR.Class1 cal = new YEAR.Class1();
+            int inputAge = 30;
+            int expect = 1988;
+
+            Assert.AreEqual(expect, cal.GetBirthYear(inputAge, YEAR.Class1.YearFormat.W));
+        }
+    }
+}

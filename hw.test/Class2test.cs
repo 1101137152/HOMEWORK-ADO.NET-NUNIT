4330af89f28da423c031a82ec2ce493c08a4b74b
1101137152 <1101137152@gm.kuas.edu.tw>
2015-04-10 17:57:35 +0800

    fourth

---
+++ hw.test/Class2test.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw.Test
+{
+    [TestFixture]
+    public class Class2test
+    {
+        string ID = "";
+
+        [TestFixtureSetUp]
+        public void Initial()
+        {
+            string sql = @"INSERT INTO [dbo].[Employees]([Name],[Age])
+                             VALUES
+                               (@Name
+                               ,@Age);SELECT CAST(scope_identity() AS int);";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["studentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@Name", "testName"));
+                    cmd.Parameters.Add(new SqlParameter("@Phone", "09000000000"));
+                    cmd.Parameters.Add(new SqlParameter("@Address", "HelloWorld"));
+                    ID = cmd.ExecuteScalar().ToString();
+
+                    Console.WriteLine(ID);
+                }
+            }
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["studentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select Max(StudentId) from Studentdata";
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while ((dr.Read()))
+                        {
+                            Assert.AreEqual(ID, dr.GetSqlInt32(0).ToString());
+                            break;
+                        }
+                        dr.Close();
+                    }
+                }
+            }
+        }
+    }
+}
